71b49e47a273ccd804950b1bd0701a0f8e50159d
SWJ <petterwooju@hanmail.net>
2021-07-25 15:01:07 +0900

    #2.0 Creating your first react component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Potato from './Potato';
+
 
 function App() {
-  return <div>Hello!!! </div>
+  return(
+     <div>
+      <h1>Hello!!! </h1>
+      <Potato />
+    </div>
+  );
 }
 
 export default App;
