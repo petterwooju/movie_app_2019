097815a8d1b2749447eff06d26945feb49b12713
SWJ <petterwooju@hanmail.net>
2021-07-25 15:44:56 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react'
 
-function Food({fav}){
-  return (<h1>I love {fav}! </h1>);
-  
+function Food({ name, picture }) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} />
+  </div>
 }
+
+const foodILike = [
+  {
+    name: "kimchi",
+    image: "https://www.maangchi.com/wp-content/uploads/2014/06/kimchi_cabbagesalting.jpg"
+  },
+  {
+    name: "kimchi_two",
+    image: "http://res.heraldm.com/content/image/2013/08/23/20130823000927_0.jpg"
+  }
+];
+
 function App() {
   return (
     <div>
-      <h1>Hello!!! </h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="sam" />
-      <Food fav="kimbam" />
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
     </div>
   );
 }
