d0429774c67dbc57627d1975d3e904769e26eeb9
SWJ <petterwooju@hanmail.net>
2021-07-25 15:57:53 +0900

    #2.3 map Recap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import React from 'react'
 function Food({ name, picture }) {
   return <div>
     <h2>I like {name}</h2>
-    <img src={picture} />
+    <img src={picture} alt={name}/>
   </div>
 }
 
 const foodILike = [
   {
+    id: 1,
     name: "kimchi",
     image: "https://www.maangchi.com/wp-content/uploads/2014/06/kimchi_cabbagesalting.jpg"
   },
   {
+    id: 2,
     name: "kimchi_two",
     image: "http://res.heraldm.com/content/image/2013/08/23/20130823000927_0.jpg"
   }
@@ -21,7 +23,7 @@ const foodILike = [
 function App() {
   return (
     <div>
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} />)}
     </div>
   );
 }
